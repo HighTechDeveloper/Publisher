165d52791e1e12c06ea4724a255d005a3028ed50
Максим <verstak80@yandex.ru>
2023-10-01 14:16:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+using RabbitMQ.Client;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+
+
+
+using (var connection = factory.CreateConnection())
+{
+    using (var chanel = connection.CreateModel())
+    {
+        chanel.QueueDeclare(queue: "first",
+            exclusive: false,
+            durable: true,
+            autoDelete: false,
+            arguments: null);
+
+        var message = "4 message";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        chanel.BasicPublish(exchange: "",
+                     routingKey: "first",
+                     basicProperties: null,
+                     body: body);
+
+        Console.WriteLine("Done");
+    }
+}
